c1544945f67968e20ab511aea5d938ddda254485
DTEQ46 <olasehindedaniel109@gmail.com>
2022-09-14 08:00:11 -0700

    check alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * This function checks its argument
+ * if it is a letter, it returns 1
+ * else, it returns 0
+ * it uses ASCII to perform
+ * this operation
+*/
+int print_sign(int n)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
