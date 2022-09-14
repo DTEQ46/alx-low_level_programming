c472ae8df56e8b2141059644f4d2063a6992182f
DTEQ46 <olasehindedaniel109@gmail.com>
2022-09-14 08:09:56 -0700

    check alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,9 +6,9 @@
  * it uses ASCII to perform
  * this operation
 */
-int _isalpha(int n)
+int _isalpha(int c)
 {
-	if ((n >= 97 && n <= 122) || (n >= 65 && n <= 90))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
